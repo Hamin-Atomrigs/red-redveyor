1473f8add9a20f861d5e4500e699f7b03b626b3a
JAEKYUNG KIM <memo1239@naver.com>
2023-08-08 15:12:26 +0900

    init

--- src/app/components/landing/fifth/fifth.js
+++ src/app/components/landing/fifth/fifth.js
@@ -2,6 +2,31 @@ export default function FifthLanding() {
   return (
     <>
       <div id="#contactUs">This is fifth Part of landing page</div>
+      <div className="flex flex-row justify-center">
+        <div>
+        <div>
+          email
+        </div>
+        <div>
+          telephone
+        </div>
+        <div>
+          fax
+        </div>
+        <div>
+          business hour
+        </div>
+      </div>
+      <div>
+        <div>
+          address
+        </div>
+        <div>
+          map
+        </div>
+        </div>
+      </div>
+
     </>
   );
 }
