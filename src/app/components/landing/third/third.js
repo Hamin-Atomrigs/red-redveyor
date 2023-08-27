526ee9c10338b71b17f67f56d13146697c560ab9
hamink <hamin@atomrigs.io>
2023-08-27 18:30:55 +0900

    nav & third landing

--- src/app/components/landing/third/third.js
+++ src/app/components/landing/third/third.js
@@ -1,7 +1,81 @@
+import { Nunito_Sans } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const portfolios = [
+  {
+    num: '01',
+    name: 'Project name',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    pics: '/first-project.png',
+    tags: '#hashtags',
+  },
+  {
+    num: '02',
+    name: 'Project name',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    pics: '/second-project.png',
+    tags: '#hashtagsss',
+  },
+];
+
+const headerFont = Nunito_Sans({
+  weight: '900',
+  style: 'normal',
+  subsets: ['latin'],
+});
+
+const subHeaderFont = Nunito_Sans({
+  weight: '700',
+  style: 'normal',
+  subsets: ['latin'],
+});
+
+const descFont = Nunito_Sans({
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+});
+
 export default function ThirdLanding() {
   return (
     <>
-      <div id="portfolio"></div>
+      <div id="portfolio">
+        <div className="text-center">
+          <div className={[headerFont.className, 'text-4xl', 'mb-6'].join(' ')}>포트폴리오</div>
+        </div>
+        <div className="flex flex-row justify-center mt-32 mb-20 max-w-6xl mx-auto">
+          <div className="flex flex-row max-w-lg pr-14">
+            <div className={[subHeaderFont.className, 'text-7xl', 'text-[#333333]', 'opacity-20', 'pr-6'].join(' ')}>{portfolios[0].num}</div>
+            <div>
+              <div className={[subHeaderFont.className, 'text-2xl'].join(' ')}>{portfolios[0].name}</div>
+              <div className={[descFont.className, 'text-xl', 'pt-1.5'].join(' ')}>{portfolios[0].desc}</div>
+              <div>
+                <Image className="pt-5 pb-4" src={portfolios[0].pics} width={358} height={358} alt="first-portfolio" />
+              </div>
+              <div className="rounded-3xl bg-[#D9D9D9] inline-block">
+                <span className={[descFont.className, 'text-base', 'text-[#333333]', 'opacity-80', 'px-2.5'].join(' ')}>{portfolios[0].tags}</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row max-w-lg">
+            <div className={[subHeaderFont.className, 'text-7xl', 'text-[#333333]', 'opacity-20', 'pr-6'].join(' ')}>{portfolios[1].num}</div>
+            <div>
+              <div className={[subHeaderFont.className, 'text-2xl'].join(' ')}>{portfolios[1].name}</div>
+              <div className={[descFont.className, 'text-xl', 'pt-1.5'].join(' ')}>{portfolios[1].desc}</div>
+              <div>
+                <Image className="pt-5 pb-4" src={portfolios[1].pics} width={358} height={358} alt="second-portfolio" />
+              </div>
+              <div className="rounded-3xl bg-[#D9D9D9] inline-block">
+                <span className={[descFont.className, 'text-base', 'text-[#333333]', 'opacity-80', 'px-2.5'].join(' ')}>{portfolios[1].tags}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link href="/portfolio">
+          <div className={[descFont.className, 'text-xl', 'text-[#333333]', 'underline', 'underline-offset-1', 'max-w-6xl', 'mx-auto', 'text-end'].join(' ')}>Check more projects</div>
+        </Link>
+      </div>
     </>
   );
 }
--- src/app/components/nav/nav.js
+++ src/app/components/nav/nav.js
@@ -18,12 +18,7 @@ export default function Nav() {
   return (
     <>
       <nav>
-        <div className="flex h-36 w-screen">
-          <div className="flex items-center justify-center w-3/12">
-            <Link href={'/'}>
-              <Image className="h-12" src="/logo.png" width={250} height={50} alt="logo" />
-            </Link>
-          </div>
+        <div className="flex h-36 w-screen justify-center">
           <div className="flex w-6/12 gap-x-11 justify-center items-center">
             <Link className={[navFont.className, 'w-24', 'text-center'].join(' ')} href={`${pathName === '/' ? '#services' : '/#services'}`}>
               서비스
@@ -31,15 +26,15 @@ export default function Nav() {
             <Link className={[navFont.className, 'w-24', 'text-center'].join(' ')} href={`${pathName === '/' ? '#portfolio' : '/#portfolio'}`}>
               포트폴리오
             </Link>
+            <Link href={'/'}>
+              <Image src="/main-logo.png" width={162} height={77} alt="logo" />
+            </Link>
             <Link className={[navFont.className, 'w-24', 'text-center'].join(' ')} href={`${pathName === '/' ? '#products' : '/#products'}`}>
               제품
             </Link>
             <Link className={[navFont.className, 'w-24', 'text-center'].join(' ')} href={`${pathName === '/' ? '#contactUs' : '/#contactUs'}`}>
               연락처
             </Link>
-          </div>
-          <div className="flex w-3/12 justify-center items-center quote">
-            <Button height="h-10" width="w-36" name="견적문의" link="/quote" />
           </div>
         </div>
       </nav>
