1e6d00f224d6b05b34c77bb1cbf5d352f484cf3f
hamink <95592054+hamink@users.noreply.github.com>
2023-10-02 22:38:47 -0600

    add padding

--- src/app/components/landing/first/first.js
+++ src/app/components/landing/first/first.js
@@ -7,14 +7,14 @@ export default function FirstLanding() {
   return (
     <>
       <div className="bg-cover" style={{ backgroundImage: 'url(/landing-first-part-bg.svg)' }}>
-        <div className="max-w-xs flex flex-row py-11 md:py-15 2xl:py-24 mx-auto md:max-w-3xl md:justify-center 2xl:max-w-7xl" >
+        <div className="max-w-xs flex flex-row py-11 md:py-15 2xl:py-24 mx-auto md:gap-x-3 md:max-w-3xl md:justify-center 2xl:max-w-7xl 2xl:gap-x-8" >
           <div className="hidden md:block md:w-36 md:h-60 md:mr-2 2xl:w-64 2xl:h-[616px] relative">
             <Image className="rounded-3xl object-cover" src="/first-section-belt.svg" alt="first-section-belt" fill />
           </div>
           <div className="w-40 h-52 md:w-52 md:h-60 2xl:w-96 2xl:h-[616px] relative">
             <Image className="rounded-3xl object-cover" src="/second-section-belt.png" alt="second-section-belt" fill />
           </div>
-          <div className="w-44 pl-6 md:w-80 md:pl-12 2xl:w-[550px] 2xl:pl-20 flex flex-col justify-center">
+          <div className="w-52 pl-6 md:w-96 md:pl-12 2xl:w-[550px] 2xl:pl-20 flex flex-col justify-center">
             <div className={[handWriteFont.className, 'text-cyan', 'text-[8px]', 'md:text-[14px]', '2xl:text-[20px]'].join(' ')}>the best conveyor services</div>
             <div className={[headerFont.className, 'text-xl', 'md:text-3xl', '2xl:text-5xl'].join(' ')}>컨베이어</div>
             <div className={[headerFont.className, 'text-xl', 'md:text-3xl', '2xl:text-5xl', 'mb-1', '2xl:mb-7'].join(' ')}>제작의 최강자</div>
