fbb1d572c26dc65fc081670f8c46f1ca2d6c24ee
hamink <95592054+hamink@users.noreply.github.com>
2023-10-10 01:32:53 -0600

    add test api

--- src/app/api/submit/route.js
+++ src/app/api/submit/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
+export async function GET() {
+    return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
 export async function POST(request) {
     const formData = await request.json();
     console.log(formData);
