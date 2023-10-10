8d6f5ff5a841a3ac10c1cb2d7aae18749afbde64
hamink <95592054+hamink@users.noreply.github.com>
2023-10-10 01:59:25 -0600

    add googletags

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import Script from 'next/script'
 import FirstLanding from './components/landing/first/first';
 import SecondLanding from './components/landing/second/second';
 import ThirdLanding from './components/landing/third/third';
@@ -9,6 +10,16 @@ import './globals.css';
 export default function Home() {
   return (
     <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <FirstLanding style={{ marginLeft: '30px', marginRight: '30px' }} />
       <SecondLanding style={{ marginLeft: '30px', marginRight: '30px' }} />
       <ThirdLanding />
