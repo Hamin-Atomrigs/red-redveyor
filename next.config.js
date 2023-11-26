fcdd79c1e7df24e8c86aa887390d3eef49d7ac9c
Hamin-Atomrigs <hamin@atomrigs.io>
2023-11-26 14:17:52 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/(.*)',
+                has: [
+                    {
+                        type: 'host',
+                        value: '(www.)?redconveyor.com',
+                    },
+                ],
+                permanent: true,
+                destination: 'https://www.redconveyor.com/:path*',
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
